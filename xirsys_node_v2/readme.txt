8305feb2e38036c08ca4ff3497e6f8097f93dd9e
Jerry Chabolla <jerry@influxis.com>
2016-08-15 13:26:50 -0700

    Added v2 files

--- xirsys_node_v2/readme.txt
+++ xirsys_node_v2/readme.txt
@@ -1,45 +1,37 @@
-# xirsys-nodejs
-Querying the Xirsys API with Nodejs
-
-## About V2
 
 This is an example using Nodejs to securely query the Xirsys API from a serverside counterpart.  Although we are using Nodejs in this example you can also use other technologies like PHP, Java C# and the sort.
 
-
-## Start
-
 1.  Download and install the express module for Node.js globaly or in the root of the project.
 
-```
-npm install express
-```
+	ex:  npm install express
 
 2.  Open the server.js file included in the download, in any text editor.
 
 3.  Update the credentials for your account at the top of the document adding your ident, your secret token and domain information.
-```
+
+ex:
+
 	var ident = 'jerrychabolla';
 	var secret = 'iasaiw42o20m240204ij202r0fifjd';
 	var domain = 'www.mydomain.com';
 	var application = 'default';// '[Optional Application]'
 	var room = 'default';//'[Optional Room]';
-```
+
 
 4.  Add your secure certificate and key or create a self signed one.
-```
-key:fs.readFileSync(path.join( __dirname, 'server.key')),
-cert:fs.readFileSync(path.join( __dirname, 'server.crt'))
-```
+
+ex:
+	
+	key:fs.readFileSync(path.join( __dirname, 'server.key')),
+    cert:fs.readFileSync(path.join( __dirname, 'server.crt'))
 
 5.  From Terminal or a DOS prompt run the server file.
-```
-node server
-```
+
+	ex:  node server
+
 6.  Open a browser window and enter https://localhost:8000/test.html 
+	NOTE:  If using a self signed certificate you will get a warning and you will have to hit proceed from the browser to continue. 
 
-NOTE:  If using a self signed certificate you will get a warning and you will have to hit proceed from the browser to continue. 
-
-## Using the GUI
 
 Click on each button in the GUI to tell node to return the information.  If your info was entered correctly you should see the data populate within the text fields above each button respectfully.
 
---
+++ xirsys_node_v2/server.js
@@ -0,0 +1,153 @@
+var express = require('express');
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
+var qs = require('querystring');
+
+var app = express();
+var srvPort = 8000;
+
+var rtcAPI = 'service.xirsys.com';
+
+// API Details - See Your Xirsys Account for Details
+var ident = '[Your Invent Ident (not email)]';
+var secret = '[Your Secret Token]';
+var domain = '[www.YourDomain.com]';
+var application = 'default';// '[Optional Application]'
+var room = 'default';//'[Optional Room]';
+
+//SSL certificate.  Webrtc needs to be in a secure domain to do Audio / Video Sharing.
+var opts = {
+    key:fs.readFileSync(path.join( __dirname, 'server.key')),
+    cert:fs.readFileSync(path.join( __dirname, 'server.crt'))
+};
+
+var server = https.createServer(opts, app).listen(srvPort);
+
+console.log('Server on Port '+srvPort);
+
+app.use(function(req, res, next){
+    console.log('req: ',req.method,' for: ',req.url);
+    next();
+});
+
+app.use(express.static('./public'));
+
+//Returns Secure token to connect to the service.
+app.post('/webrtc/signal/token', function(req, res){
+    console.log('GET SIGNAL TOKEN!');
+    body = '';
+    req.on('data', function(d){
+        body += d;
+    });
+    req.on('end', function(){
+        var username = qs.parse(body).username;
+        console.log('User request RTC Token username: ',username);
+
+        var o = {
+            ident: ident,
+            secret: secret,
+            domain: domain,
+            application: application,
+            room: room,
+            secure: '1',
+            username: username
+        };
+        bodyString = JSON.stringify(o);
+        //console.log('User request RTC Token data: ',bodyString);
+        options = {
+            host: rtcAPI,
+            path: '/signal/token',
+            method: 'GET',
+            headers: {
+                'Content-Type':'application/json',
+                'Content-Length':bodyString.length
+            }
+        };
+        //get RTC token for user, send token back in response.
+        https.request(options, function(httpres) {
+            var str = '';
+            httpres.on('data', function(data){ str += data; });
+            httpres.on('error', function(e){ console.log('error: ',e); });
+            httpres.on('end', function(){ 
+                console.log("RTC Token! "+req.url,str); 
+                res.writeHead(200, 'Content-Type', 'application/json');
+                res.end(str);
+            });
+        }).write(bodyString);
+    });
+});
+
+//Returns List of valid signaling servers that the clients can connect to.
+app.get('/webrtc/signal/list',function(req, res){
+    console.log("GET SIGNALS LIST!");
+
+    var o = {
+        ident: ident,
+        secret: secret,
+        domain: domain,
+        application: application,
+        room: room,
+        secure: '1'
+    };
+    bodyString = JSON.stringify(o);
+    //console.log('User request RTC List data: ',bodyString);
+
+    options = {
+        host: rtcAPI,
+        path: '/signal/list',
+        method: 'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'Content-Length':bodyString.length
+        }
+    };
+    //get RTC server List for user.
+    https.request(options, function(httpres) {
+        var str = '';
+        httpres.on('data', function(data){ str += data; });
+        httpres.on('error', function(e){ console.log('error: ',e); });
+        httpres.on('end', function(){ 
+            console.log("RTC Server List!", str);
+            res.writeHead(200, 'Content-Type', 'application/json');
+            res.end(str);
+        });
+    }).write(bodyString);
+});
+
+//Returns a Valid ICE server setup to handle the WebRTC handshake and TURN connection if needed.
+app.get('/webrtc/ice',function(req, res){
+    console.log("GET ICE LIST!");
+    var o = {
+        ident: ident,
+        secret: secret,
+        domain: domain,
+        application: application,
+        room: room,
+        secure: '1'
+    };
+    bodyString = JSON.stringify(o);
+
+    console.log('User request ICE: ',bodyString);
+
+    options = {
+        host: rtcAPI,
+        path: '/ice',
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Content-Length':bodyString.length
+        }
+    };
+    //get RTC server List for user.
+    https.request(options, function(httpres) {
+        var str = '';
+        httpres.on('data', function(data){ str += data; });
+        httpres.on('error', function(e){ console.log('error: ',e); });
+        httpres.on('end', function(){ 
+            console.log("ICE String!", str);
+            res.writeHead(200, 'Content-Type', 'application/json');
+            res.end(str);
+        });
+    }).write(bodyString);
+});
